24d7a719c4c201c9a018bf6803a86c56bb81c812
bckyrd <host@bckyrd.io>
2023-09-20 20:33:45 -0700

    registration done

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 import os
 import uuid
 from fastapi.middleware.cors import CORSMiddleware
+from enum import Enum
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
+from pydantic import BaseModel
+from passlib.context import CryptContext
+from typing import Optional
 
 app = FastAPI()
-
 
 # Configure CORS
 origins = ["*"]  # Update with the origin of your web page
@@ -37,19 +39,17 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 
-
-# app/models.py
+# Define the database model for CapturedImage
 Base = declarative_base()
 
 class CapturedImage(Base):
     __tablename__ = 'captured_images'
 
     id = Column(Integer, primary_key=True, index=True)
-    filename = Column(String(length=255), index=True)  # Specify the length (e.g., 255)
+    filename = Column(String(length=255), index=True)
     timestamp = Column(DateTime, default=datetime.utcnow)
 
-# Create the database table
-Base.metadata.create_all(bind=engine)
+
 
 @app.post("/capture")
 async def capture_image(file: UploadFile = File(...)):
@@ -93,4 +93,67 @@ async def get_images():
     return [{"id": image.id, "filename": image.filename, "timestamp": image.timestamp} for image in images]
 
 
+
+# Define the User model
+class UserRoleEnum(str, Enum):
+    admin = "admin"
+    user = "user"
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(length=50), unique=True, index=True)
+    hashed_password = Column(String(length=60))
+    role = Column(String(length=60), default="user")  # Store role as a string
+    
+
+# Password Hashing
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+# Pydantic model for user registration
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    role: Optional[UserRoleEnum] = UserRoleEnum.user  # Default to "user"
+
+# Registration endpoint
+@app.post("/register")
+async def register_user(user: UserCreate):
+    try:
+        # Check if username already exists
+        db = SessionLocal()
+        db_user = db.query(User).filter(User.username == user.username).first()
+        if db_user:
+            db.close()
+            raise HTTPException(status_code=400, detail="Username already exists")
+
+        # Hash the password
+        hashed_password = hash_password(user.password)
+
+        # Create a new user with the specified user role
+        db_user = User(username=user.username, hashed_password=hashed_password, role=user.role)
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        db.close()
+
+        return {"message": "User registered successfully"}
+    except Exception as e:
+        # Log any exceptions for debugging purposes
+        print("Error:", str(e))
+        raise HTTPException(status_code=500, detail="User registration error")
+
+
+
+# Create the database table
+Base.metadata.create_all(bind=engine)
 
