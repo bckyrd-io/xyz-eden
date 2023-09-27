e3eb48a1bdd549d7f123e3c1bf9555bf2348f995
bckyrd <host@bckyrd.io>
2023-09-27 08:10:37 -0700

    login function working

--- api/main.py
+++ api/main.py
@@ -1,13 +1,16 @@
-# main.py
-from fastapi import FastAPI, UploadFile, Depends, HTTPException, status
+from fastapi import FastAPI, UploadFile, Depends, HTTPException, status, Form
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session  # Add this import
 from typing import Optional
 from datetime import datetime, timedelta
 from pydantic import BaseModel
 from jose import JWTError, jwt
 from capture import capture_and_save_image
 from users import create_user, get_user_by_username, UserRoleEnum, hash_password
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from users import authenticate_user, get_current_user
+from models import SessionLocal, User  # Add this import
 
 app = FastAPI()
 
@@ -24,22 +27,15 @@ app.add_middleware(
 # ... Other middleware and configuration ...
 
 # Endpoint to capture and save images
-
-
 @app.post("/capture")
 async def capture_image(file: UploadFile = UploadFile(...)):
     return capture_and_save_image(file)
 
-# ... Other endpoints ...
-
 # Registration endpoint
-
-
 class UserCreate(BaseModel):
     username: str
     password: str
     role: Optional[UserRoleEnum] = UserRoleEnum.user
-
 
 @app.post("/register")
 async def register_user(user: UserCreate):
@@ -48,6 +44,59 @@ async def register_user(user: UserCreate):
     del user_data["password"]
     return create_user(user_data)
 
-# ... Other endpoints ...
+# Login endpoint
+@app.post("/login")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_token = await authenticate_user(form_data.username, form_data.password)
+    if user_token is None:
+        raise HTTPException(
+            status_code=401, detail="Incorrect username or password")
+    return user_token
 
-# Define the remaining endpoints and dependencies as needed.
+# Edit user role endpoint (Requires admin privileges)
+@app.put("/user/{user_id}/edit-role")
+async def edit_user_role(user_id: int, new_role: UserRoleEnum, current_user: str = Depends(get_current_user)):
+    db = SessionLocal()
+    user = db.query(User).filter(User.username == current_user).first()
+    db.close()
+
+    if user.role != UserRoleEnum.admin:
+        raise HTTPException(
+            status_code=403, detail="Only admin users can edit roles")
+
+    db = SessionLocal()
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        db.close()
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db_user.role = new_role
+    db.commit()
+    db.close()
+
+    return {"message": "User role updated successfully"}
+
+# Delete user endpoint (Requires admin privileges)
+@app.delete("/user/{user_id}/delete")
+async def delete_user(user_id: int, current_user: str = Depends(get_current_user)):
+    db = SessionLocal()
+    user = db.query(User).filter(User.username == current_user).first()
+    db.close()
+
+    if user.role != UserRoleEnum.admin:
+        raise HTTPException(
+            status_code=403, detail="Only admin users can delete users")
+
+    db = SessionLocal()
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        db.close()
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(db_user)
+    db.commit()
+    db.close()
+
+    return {"message": "User deleted successfully"}
+
+# ... Define other endpoints and dependencies as needed ...
--- api/users.py
+++ api/users.py
@@ -1,9 +1,17 @@
-# users.py
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
 from passlib.context import CryptContext
+from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 from models import SessionLocal, User
-from enum import Enum 
+from enum import Enum
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer
+
+SECRET_KEY = "24f8c5e5-fcb5-bdfc-aac0-7bff0e800334"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 class UserRoleEnum(str, Enum):
     admin = "admin"
@@ -16,6 +24,32 @@ def hash_password(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# users.py
+async def authenticate_user(username: str, password: str):
+    db = SessionLocal()
+    user = db.query(User).filter(User.username == username).first()
+    db.close()
+
+    if not user or not verify_password(password, user.hashed_password):
+        return None
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return access_token
+
 
 def create_user(user_data):
     try:
@@ -35,6 +69,17 @@ def get_user_by_username(username: str):
     user = db.query(User).filter(User.username == username).first()
     db.close()
     return user
+
+# Add the get_current_user function here
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=400, detail="Token validation error")
+        return username
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token validation error")
 
 def edit_user_role(user_id: int, new_role: UserRoleEnum, current_user: str):
     # Implement the edit_user_role functionality here
